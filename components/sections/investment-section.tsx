0a4574decc65566b2ad69149b85d2a86c3482540
KaczmarekBartosz <bartosz.kaczmarek@icloud.com>
2025-10-03 18:40:07 +0200

    Poprawka zdjęcia

--- components/sections/investment-section.tsx
+++ components/sections/investment-section.tsx
@@ -92,7 +92,7 @@ export function InvestmentSection() {
           <div className="space-y-8 flex flex-col">
             <div className="overflow-hidden rounded-3xl">
               <Image
-                src="/investment-image-green.jpg"
+                src="/galeria/8.jpg"
                 alt="Wizualizacja osiedla Domy z Przyszłością z dużą ilością zieleni"
                 width={1200}
                 height={800}
